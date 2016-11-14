31f279a182a8574fa7bd8ba84c729c79c8e79839
User <user@mac-mini-3136.local>
2016-11-14 19:19:06 +0400

    Initial Commit

---
+++ RSS-reader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSS-reader
+//
+//  Created by User on 14/11/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
